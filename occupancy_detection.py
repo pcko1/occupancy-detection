86c97c67f156b614b3a4dbce746bfdea97e41d19
pcko1 <kotsias.pan@gmail.com>
2018-06-19 02:00:20 +0200

    Added scoring functions.

--- occupancy_detection.py
+++ occupancy_detection.py
@@ -1,7 +1,6 @@
 from pandas import read_csv, DatetimeIndex
-from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, roc_curve
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, roc_curve, auc
 from matplotlib import pyplot as plt
 
 # Import data from CSV/txt file as Pandas DataFrame
@@ -34,12 +33,14 @@ x_test2 = df_test2.values[:,:-1]
 y_test2 = df_test2.values[:, -1]
 
 # Train the tree
-#clf = tree.DecisionTreeClassifier()
-clf = RandomForestClassifier(max_depth=10, random_state=0)
+clf = RandomForestClassifier(n_estimators=10,max_depth=2, random_state=0)
 clf = clf.fit(x_train,y_train)
 
 # Test the model by predicting the label of the test datasets
 y_test1_pred = clf.predict(x_test1)
+
+# Show feature importance
+importance = clf.feature_importances_
 
 # Evaluate the model
 # Calculate confusion matrix
@@ -52,4 +53,19 @@ acc = accuracy_score(y_test1, y_test1_pred)
 f1 = f1_score(y_test1, y_test1_pred)
 
 # Calculate ROC
-roc = roc_curve(y_test1, y_test1_pred)
+fpr, tpr, thresholds = roc_curve(y_test1, y_test1_pred)
+
+# Calculate AUROC
+auroc = auc(fpr,tpr)
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % auroc)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
